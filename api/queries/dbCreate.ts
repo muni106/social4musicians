86d2d4160a3aa65a6e8cc414bb828a22a30f9419
Mounir <mounir.samite@gmail.com>
2023-09-06 20:21:00 +0200

    fix relations

--- api/queries/dbCreate.ts
+++ api/queries/dbCreate.ts
@@ -31,11 +31,63 @@ export async function userCreation(
   console.log(user);
 }
 
+export async function followGenre(
+  prisma: PrismaClient,
+  nickName: string,
+  genreName: string
+) {
+  try {
+    //verify musician existence
+    const musician = await prisma.musician.findUnique({
+      where: { nickname: nickName }, // Replace 1 with the actual ID of the EntityA
+      include: { follow: true }, // Include the existing EntityBs related to EntityA
+    });
+    if (!musician) {
+      console.error("musician not found");
+      return;
+    }
+
+    //verify genre existence
+    const existingGenre = await prisma.genre.findUnique({
+      where: {
+        genrename: genreName,
+      },
+    });
+    if (!existingGenre) {
+      console.error("genre not found");
+      return;
+    }
+
+    const newFollow = await prisma.follow.create({
+      data: {
+        nickname: nickName,
+        genrename: genreName,
+      },
+    });
+
+    // Step 3: Add the new EntityB to the collection of EntityBs related to EntityA
+    const updatedUser = await prisma.musician.update({
+      where: { nickname: nickName },
+      data: {
+        follow: {
+          connect: [
+            {
+              genrename_nickname: newFollow,
+            },
+          ],
+        },
+      },
+    });
+
+    console.log("EntityB added to EntityA:", updatedUser);
+  } catch (error) {
+    console.error("Error adding EntityB to EntityA:", error);
+  }
+}
 
 // CHAT CREATIONS
 
-
-// POSTS CREATIONS 
+// POSTS CREATIONS
 
 export async function createDiscussion(
   prisma: PrismaClient,
@@ -46,13 +98,27 @@ export async function createDiscussion(
 ) {
   const discussion = await prisma.discussion.create({
     data: {
-        timestampdiscussion: timeStamp,
-        title: title,
-        average: average,
-        nickname: nickname
+      timestampdiscussion: timeStamp,
+      title: title,
+      average: average,
+      nickname: nickname,
     },
   });
-  return discussion;
+
+  const user = await prisma.musician.update({
+    where: {
+      nickname: nickname,
+    },
+    data: {
+      discussion: {
+        connect: {
+          discussionid: discussion.discussionid,
+        },
+      },
+    },
+  });
+
+  return user;
 }
 
 export async function createComment(
@@ -64,14 +130,70 @@ export async function createComment(
 ) {
   const comment = await prisma.comment.create({
     data: {
-        commenttext: text,
-        nickname: sender,
-        timestampcomment: timeStamp,
-        discussionid: discussion
+      commenttext: text,
+      nickname: sender,
+      timestampcomment: timeStamp,
+      discussionid: discussion,
     },
   });
-  return comment;
+
+  const userComment = await prisma.musician.update({
+    where: {
+      nickname: sender,
+    },
+    data: {
+      comment: {
+        connect: {
+          commentid: comment.commentid,
+        },
+      },
+    },
+  });
+  return userComment;
 }
 
+export async function createReaction(
+  prisma: PrismaClient, 
+  nickName: string,
+  discussionID: number,
+  vote: boolean,
+  timeStamp: Date
+  ) { 
+    const reactionVal = await prisma.reaction.create({
+      data: {
+        nickname: nickName,
+        discussionid: discussionID,
+        vote: vote,
+        dateandtime: timeStamp
+      }
+    });
 
-//band part 
+    const discussion = await prisma.discussion.update({
+      where: {
+        discussionid: discussionID,
+      },
+      data: {
+        reaction: {
+          connect: {
+            nickname_discussionid: reactionVal
+          }
+        }
+      }
+    });
+
+    const user = await prisma.musician.update({
+      where: {
+        nickname: nickName
+      },
+      data: {
+        reaction: {
+          connect: {
+            nickname_discussionid: reactionVal
+          }
+        }
+      }
+    });
+    return discussion;
+}
+
+//band part
