b3da57b78739a63272e8d19916aabb253cfb3301
Mounir Samite <mounir.samite@gmail.com>
2024-02-11 15:17:05 +0100

    start attaching server to frontend

---
+++ api/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+
+
+export const router = Router();
+
+router.use('')
+router.use()
--- api/server.ts
+++ api/server.ts
@@ -1,15 +1,30 @@
-import express, {Express} from 'express';
-import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
+import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { router } from "./routes";
 
 const prisma = new PrismaClient();
-const app:Express = express();
+const app: Express = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 4000;
 
+app.get("/api/data", (req: Request, res: Response) => {
+  const responseData = { message: "Hello from the server! " };
+  res.json(responseData);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+  console.log("ciao");
+  console.log(`Server is running on port: ${PORT}`);
 });
